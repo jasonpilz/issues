7b5916e3c80ef9aaba231e230f7050b045ec3b70
Jason Pilz <jasonpilz@gmail.com>
2016-10-22 22:19:42 -0700

    Add initial tests for cli module

--- test/issues/cli_test.exs
+++ test/issues/cli_test.exs
@@ -4,4 +4,16 @@ defmodule CliTest do
 
   import Issues.CLI, only: [ parse_args: 1 ]
 
+  test ":help is returned by option parsing with -h and --help options" do
+    assert parse_args(["-h",     "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "three values returned if three given" do
+    assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
+  end
+
+  test "count is defaulted if 2 arguments given" do
+    assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
 end
