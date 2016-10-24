e4d000132ea45278d2e79b7e5ee6b8e96884626c
Jason Pilz <jasonpilz@gmail.com>
2016-10-23 20:41:17 -0700

    Add process/1 and process/3 to CLI interface

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,6 +8,7 @@ defmodule Issues.CLI do
 
   def run(argv) do
     parse_args(argv)
+    |> process
   end
 
   @doc """
@@ -27,5 +28,16 @@ defmodule Issues.CLI do
       { _, [ user, project ], _ }        -> { user, project, @default_count }
       _                                  -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage:  issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
