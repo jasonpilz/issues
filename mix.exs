5f61b039198487652005c211bb20d81d223b37b2
Jason Pilz <jasonpilz@gmail.com>
2016-10-26 19:39:46 -0700

    Add escript config for mix

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.Mixfile do
   def project do
     [
       app:             :issues,
+      escript:         escript_config,
       version:         "0.1.0",
       elixir:          "~> 1.3",
       build_embedded:  Mix.env == :prod,
@@ -13,25 +14,18 @@ defmodule Issues.Mixfile do
   end
 
   # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
        httpoison: "~> 0.9",
        poison:    "~> 1.5"
     ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
